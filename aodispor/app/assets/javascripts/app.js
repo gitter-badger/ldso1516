883016e96b7b7f35d718c7977921f36962f92266
HugoFAC <ei10073@fe.up.pt>
2015-11-18 17:47:15 +0000

    Modify HomePage
    Still very incomplete. buton to support and sign in fuctioning.
    When listing professionals is completed, this will be updated

---
+++ aodispor/app/assets/javascripts/app.js
@@ -0,0 +1,48 @@
+angular.module('aodispor', ['ui.router', 'templates', 'ng-token-auth'])
+.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$authProvider',
+    function($stateProvider, $urlRouterProvider, $authProvider) {
+        $authProvider.configure({
+            apiUrl: 'http://localhost:3000/api',
+            authProviderPaths: {
+                github: '/auth/github',
+                facebook: '/auth/facebook'
+            }
+        });
+
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'home/_home.html'
+            })
+            .state('sign_in', {
+                url: '/sign_in',
+                templateUrl: 'auth/_signIn.html',
+                controller: 'AuthCtrl'
+            })
+            .state('support', {
+                url: '/support',
+                templateUrl: 'guest/support.html'
+                //controller: 'AuthCtrl'
+            })
+            .state('edit_profile', {
+                url: '/users/edit_profile',
+                templateUrl: 'users/_edit.html',
+                controller: 'UsersCtrl',
+                resolve: {
+                    // If the user is not authenticated then it'll be redirected to the home page
+                    auth: function($auth, $state) {
+                        $auth.validateUser()
+                            .catch(function() {
+                                $state.go('home');
+                            });
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }
+]);
---
+++ aodispor/app/assets/javascripts/auth/authCtrl.js
@@ -0,0 +1,27 @@
+angular.module('aodispor')
+    .controller('AuthCtrl', function($scope, $location, $auth, $state) {
+        $scope.authenticate = function(provider) {
+            $auth.authenticate(provider);
+            .then(function() {
+                $state.go('home');
+            })
+        };
+
+
+        $scope.signOut = function() {
+            $auth.signOut()
+                .then(function() {
+                    $state.go('home');
+                })
+                .catch(function(error) {
+                    alert(error.data.errors);
+                });
+        };
+
+
+        $auth.validateUser()
+            .then(function() {
+                // If the user is signed in redirect to the home state
+                // $state.go('home');
+            });
+    });
