af35f874b4cd204f06fa9743225b28a6f61b545a
momoepro <supahmomo@gmail.com>
2015-11-18 15:41:48 +0000

    Messages will be stored in the database

--- aodispor/db/schema.rb
+++ aodispor/db/schema.rb
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20151117111952) do
     t.decimal  "price"
   end
 
+  create_table "support", force :cascade do |t|
+    t.string "name",
+    t.string "email",
+    t.string "message"    default "", null: false
+  end
+
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
