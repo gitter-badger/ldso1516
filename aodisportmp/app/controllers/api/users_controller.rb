362f71e8f373f1dc53ebe602fb27442b81995c64
André Pinheiro <andrepdpinheiro@gmail.com>
2015-11-19 12:13:09 +0000

    Fixing some errors

--- aodisportmp/app/controllers/api/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::UsersController < ActionController::API
-
-
-  def index
-    users = User.all
-    render(json: ActiveModel::ArraySerializer.new(users, each_serializer: Api::UserSerializer, root: 'users'))
-  end
-end
