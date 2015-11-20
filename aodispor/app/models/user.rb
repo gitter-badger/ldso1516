9eda519f6c79989c8a606f24e98752a65d0d3cb8
André Pinheiro <andrepdpinheiro@gmail.com>
2015-11-20 13:16:42 +0000

    Still not working

--- aodispor/app/models/user.rb
+++ aodispor/app/models/user.rb
@@ -4,12 +4,11 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-
-  attr_accessor :job, :job_description, :price
+  
   # Run validations only when user updates his/her profile
   validates_presence_of :job, :job_description, :price, on: :update
   validates :price, numericality: { greater_than_or_equal_to: 0 }, on: :update
-  #before_update :check_updated_attributes
+  before_update :check_updated_attributes
 
 
   # Allow us to do User.professionals to return all the professional users
